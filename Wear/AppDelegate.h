36a781f05f3b46bb5f6772c7feb13ca353cd2120
talisk <talisk@foxmail.com>
2015-10-11 11:17:07 +0800

    Initial Commit

---
+++ Wear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wear
+//
+//  Created by 孙恺 on 15/10/11.
+//  Copyright © 2015年 sunkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
